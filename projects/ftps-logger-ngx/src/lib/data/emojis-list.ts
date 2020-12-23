d45650436df07f7755edcf6fef2edf0343b7ef16
ftps-lj <ljones@ft-ps.com>
2020-12-23 06:47:42 -0600

    Add Emojis List in data folder

---
+++ projects/ftps-logger-ngx/src/lib/data/emojis-list.ts
@@ -0,0 +1,86 @@
+export const LIGHTMOJIS = [
+  '👻',
+  '💀',
+  '🤖',
+  '🤡',
+  '☃️',
+  '🎅🏻',
+  '⚽️',
+  '🦷',
+  '🦓',
+  '🧦',
+];
+
+export const DARKMOJIS = [
+  '🦈',
+  '🎩',
+  '🧥',
+  '🎱',
+  '🎮',
+  '💣',
+  '💩',
+  '🎬',
+  '🕋',
+  '⬛️',
+];
+
+export const REDMOJIS = [
+  '🦞',
+  '👣',
+  '😈',
+  '🥤',
+  '🍎',
+  '🚒',
+  '🏮',
+  '🥊',
+  '☎️',
+  '🍅',
+];
+
+export const YELLOWMOJIS = [
+  '🩳',
+  '🐱',
+  '🧸',
+  '🤯',
+  '📀',
+  '😀',
+  '👑',
+  '🌝',
+  '🐥',
+  '🤓',
+];
+
+export const GREENMOJIS = [
+  '🐸',
+  '👽',
+  '🚜',
+  '🧶',
+  '🌳',
+  '🎄',
+  '🐢',
+  '🥦',
+  '🧃',
+  '📗',
+];
+
+export const BLUEMOJIS = [
+  '👖',
+  '💎',
+  '📮',
+  '🌎',
+  '🧢',
+  '🐳',
+  '🐟',
+  '🥽',
+  '🌊',
+  '📮',
+];
+
+export const EMOJIS_LIST = [
+  ...LIGHTMOJIS,
+  ...REDMOJIS,
+  ...GREENMOJIS,
+  ...DARKMOJIS,
+  ...YELLOWMOJIS,
+  ...BLUEMOJIS,
+];
