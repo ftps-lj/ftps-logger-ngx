ecb20401b4e56889efe35f9659deae5764a06914
Sahil Patel <50974651+spatel475@users.noreply.github.com>
2020-12-30 13:58:14 -0600

    Update ftps-logger-ngx.service.ts

--- projects/ftps-logger-ngx/src/lib/ftps-logger-ngx.service.ts
+++ projects/ftps-logger-ngx/src/lib/ftps-logger-ngx.service.ts
@@ -94,7 +94,7 @@ export class FtpsLoggerService {
     const emojiPad = 5;
 
     console.groupCollapsed(
-      `${this.levels[level].padEnd(4, ' ')}`,
+      `${this.levels[level].padEnd(15, ' ')}`,
       randomEmoji.padStart(emojiPad, ' '),
       `[${module}]`.padEnd(padLength, ' '),
       title
